f1d18f3813adad78507b01ba9eaacc37cc198731
Isha Dubey <ishadubey36@gmail.com>
2023-08-09 15:35:45 +0530

    revision

--- Day3functions/script.js
+++ Day3functions/script.js
@@ -30,41 +30,44 @@
 
 
 
-const dynamicContent = document.getElementById("dynamic-text");
-console.log(dynamicContent)
-const phrases = ["Software Engineer...", "Dancer...", "Gym Rat..."]
+// const dynamicContent = document.getElementById("dynamic-text");
+// console.log(dynamicContent)
+// const phrases = ["Software Engineer...", "Dancer...", "Gym Rat..."]
 
-let letterIndex = 0;
-let phraseIndex = 0;
+// let letterIndex = 0;
+// let phraseIndex = 0;
 
-function printLetters(phrase){
-    if (letterIndex < phrase.length){
-        dynamicContent.textContent += phrase.charAt(letterIndex);
-        letterIndex += 1;
-        setTimeout(function(){
-            printLetters(phrase)}, 100)
-    }
-    else if(letterIndex== phrase.length){
-        clearletters()
-    }
-}
-function clearletters(){
- if(letterIndex > -1)
- { 
-    let updatedPhrase= "" ;
+// function printLetters(phrase){
+//     if (letterIndex < phrase.length){
+//         dynamicContent.textContent += phrase.charAt(letterIndex);
+//         letterIndex += 1;
+//         setTimeout(function(){
+//             printLetters(phrase)}, 100)
+//     }
+//     else if(letterIndex== phrase.length){
+//         clearletters()
+//     }
+// }
+// function clearletters(){
+//  if(letterIndex > -1)
+//  { 
+//     let updatedPhrase= "" ;
     
-    for (let index = 0; index < letterIndex; index++) {
-        updatedPhrase += phrases[phraseIndex].charAt(index);
-      }
-    dynamicContent.textContent = updatedPhrase;
-    letterIndex -= 1;
-    setTimeout (clearletters , 100);
-    }
-  else if (letterIndex == -1)
-  { phraseIndex= (phraseIndex +1) % phrases.length;
-    letterIndex = 0;
-    printLetters(phrases[phraseIndex])
-}  
+//     for (let index = 0; index < letterIndex; index++) {
+//         updatedPhrase += phrases[phraseIndex].charAt(index);
+//       }
+//     dynamicContent.textContent = updatedPhrase;
+//     letterIndex -= 1;
+//     setTimeout (clearletters , 100);
+//     }
+//   else if (letterIndex == -1)
+//   { phraseIndex= (phraseIndex +1) % phrases.length;
+//     letterIndex = 0;
+//     printLetters(phrases[phraseIndex])
+// }  
 
-}
-printLetters(phrases[phraseIndex])
+// }
+// printLetters(phrases[phraseIndex])
+
+
+Isha
