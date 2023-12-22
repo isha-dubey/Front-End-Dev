a7e04bf58370cc82536f4a74fe1de3ca95ece382
Isha Dubey <ishadubey36@gmail.com>
2023-12-22 15:21:52 +0530

    .

--- Day15/main.js
+++ Day15/main.js
@@ -24,40 +24,68 @@
 // })
 
 
-const longRunninFn = () => {
-    console.log("longRunninfn started");
-    const duration = Date.now() + 3000
-    while(Date.now() <=duration){
+// const longRunninFn = () => {
+//     console.log("longRunninfn started");
+//     const duration = Date.now() + 3000
+//     while(Date.now() <=duration){
         
-    }
-    console.log("longRunninfn ended");
-}
+//     }
+//     console.log("longRunninfn ended");
+// }
 
-const anotherFn = () => {
-    console.log("another fun Loaded");
-    longRunninFn()
-}
+// const anotherFn = () => {
+//     console.log("another fun Loaded");
+//     longRunninFn()
+// }
 
-const someFunction = () => {
-    anotherFn()
-}
+// const someFunction = () => {
+//     anotherFn()
+// }
 
-const handleClick = () => {
-    console.log("button clicked");
-}
+// const handleClick = () => {
+//     console.log("button clicked");
+// }
 
-const FuncWithtimeout = () => {
-    setTimeout(() => { 
-        console.log("hi I am the first");
+// const FuncWithtimeout = () => {
+//     setTimeout(() => { 
+//         console.log("hi I am the first");
         
-    }, 2000);
-}
+//     }, 2000);
+// }
 
-document.addEventListener("DOMContentLoaded" , ()=> {
-    document.getElementById("clickable").addEventListener("click" , handleClick)
-    FuncWithtimeout()
-    someFunction()
-})
+// document.addEventListener("DOMContentLoaded" , ()=> {
+//     document.getElementById("clickable").addEventListener("click" , handleClick)
+//     FuncWithtimeout()
+//     someFunction()
+// })
 
 
 //moving to browser storage
+
+
+// const person = localStorage.setItem('Name' , 'isha')
+// const person2 = localStorage.setItem('Name1' , 'mohit')
+
+
+// console.log(localStorage.getItem('Name'))
+// sessionStorage.clear():
+
+// // Storing data in localStorage
+// localStorage.setItem('username', 'JohnDoe');
+// localStorage.setItem('isLoggedIn', 'true');
+
+// // Retrieving data from localStorage
+// const username = localStorage.getItem('username');
+// const isLoggedIn = localStorage.getItem('isLoggedIn');
+
+// console.log(username); // Output: JohnDoe
+// console.log(isLoggedIn); // Output: true
+
+// // Removing a specific item from localStorage
+// localStorage.removeItem('username');
+
+// // Clearing all items from localStorage
+// localStorage.clear();
+
+
+
---
+++ Day15/theme.js
@@ -0,0 +1,11 @@
+
+const switchTheme = () => {
+
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const switchButton = document.getElementById("Switch-theme")
+    switchButton.addEventListener("click" , switchTheme)
+    
+})
