3ff26b910f788031dfa25a5a89fb4936dc54e986
Isha Dubey <ishadubey36@gmail.com>
2023-12-22 15:24:27 +0530

    .

--- Day15/theme.js
+++ Day15/theme.js
@@ -1,5 +1,13 @@
 
 const switchTheme = () => {
+    if(themePrefersDark.matches){
+        document.body.classList.toggle("lighttheme")
+    }
+    else {
+        document.body.classList.toggle("darktheme")
+
+    }
+    
 
 }
 
@@ -7,5 +15,7 @@ const switchTheme = () => {
 document.addEventListener("DOMContentLoaded", () => {
     const switchButton = document.getElementById("Switch-theme")
     switchButton.addEventListener("click" , switchTheme)
-    
+
+
+    const themePrefersDark = window.matchMedia("prefers-color-scheme : dark")
 })
