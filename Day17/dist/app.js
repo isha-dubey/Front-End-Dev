5a1514215c882fe2c9bd3398835deae205a66e00
Isha Dubey <ishadubey36@gmail.com>
2023-12-23 17:31:23 +0530

    .

---
+++ Day17/dist/app.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("default export")})();
---
+++ Day17/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require ("path");
+
+module.exports = {
+
+    entry : "./src/main.js" ,
+    output : {
+        filename : "app.js" ,
+        path : path.join(__dirname , "dist")
+    } ,
+    
+}
