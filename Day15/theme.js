1c62d357b2633096c53ee0b4497af7a9cb761bad
Isha Dubey <ishadubey36@gmail.com>
2023-12-29 18:19:56 +0530

    .

--- Day15/theme.js
+++ Day15/theme.js
@@ -1,27 +1,47 @@
-
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
 
-    const switchButton = document.getElementById("Switch-theme")
-    
-    const themePrefersDark = window.matchMedia("(prefers-color-scheme : dark)") 
-  
-    
+    const themePrefersDark = window.matchMedia("(prefers-color-scheme : dark)")
 
+    const getOSPreference = () => {
+        themePrefersDark.matches ? "dark" : "light" 
+    }
+
+    const getThemePrefFromLocalStorage = () => {
+        return localStorage.getItem("theme")
+    }
+
+    const checkThemePreference = () => {
+        const defaultTheme = getOSPreference()
+        const currentTheme = getThemePrefFromLocalStorage()
+        if (currentTheme == "dark") {
+            document.body.classList.toggle("darktheme")
+        }
+        else (currentTheme == "light")
+        {
+            document.body.classList.toggle("lighttheme")       
+        } setButtonText(currentTheme ?? defaultTheme )
+    }
+    const setButtonText = (theme) => {
+        switchButton.textContent = theme === "dark" ? "Light" : "Dark" ;
+
+    }
+
+    
     const switchTheme = () => {
+        let theme
         if(themePrefersDark.matches){
             document.body.classList.toggle("lighttheme");
-            //   const theme = 
+            theme = document.body.classList.contains("lighttheme")? "light" : "dark"
         }
         else {
             document.body.classList.toggle("darktheme")
+            theme = document.body.classList.contains("darktheme")? "dark" : "light"
             
-        }
-        switchButton.textContent = themePrefersDark.matches ? "Light" : "Dark" ;
-     }
+        } localStorage.setItem("theme" , theme)
+        setButtonText(theme)
+    }
     
+    const switchButton = document.getElementById("Switch-theme")
     switchButton.addEventListener("click" , switchTheme)
-
+    checkThemePreference()
 })
